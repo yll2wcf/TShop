9351edf91b773ae440b1ac2dd5ca99653f73f755
于连林520wcf <yll@520wcf.com>
2018-04-23 18:10:57 +0800

    项目分析

--- App.js
+++ App.js
@@ -3,56 +3,49 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+// 导入React 和 RN 控件
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+// 根据不同的平台加载不同的代码
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
+// ES6 语法
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
 
+    render() {
+        return ( // 渲染布局
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    这是 React Native 基础代码
+                </Text>
+
+            </View>
+        );
+    }
+}
+// 样式文件
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 30, // 字体大小
+        textAlign: 'center', // 文字居中对齐
+        color: 'red', // 字体颜色改成红色
+    }
 });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import {AppRegistry} from 'react-native';
+import App from './App'; // 引入了 App.js
 
+// 注册程序
 AppRegistry.registerComponent('TShop', () => App);
