dcc4df431c341e0fa6fcd31d19d9822e4d56e1e2
于连林520wcf <yll@520wcf.com>
2018-05-02 09:20:09 +0800

    格式化代码

--- App.js
+++ App.js
@@ -6,21 +6,11 @@
 // 导入React 和 RN 控件
 import React, {Component} from 'react';
 import {
-    Platform,
     StyleSheet,
-    Text,
     View
 } from 'react-native';
 
 import Counter from "./src/component/Counter"
-
-// 根据不同的平台加载不同的代码
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 // ES6 语法
@@ -29,9 +19,9 @@ export default class App extends Component<Props> {
     render() {
         return ( // 渲染布局
             <View style={styles.container}>
-                <Counter style={{margin:10}} initValue={1}/>
-                <Counter style={{margin:10}} initValue={2}/>
-                <Counter style={{margin:10}} initValue={3}/>
+                <Counter style={{margin: 10}} initValue={1}/>
+                <Counter style={{margin: 10}} initValue={2}/>
+                <Counter style={{margin: 10}} initValue={3}/>
             </View>
         );
     }
