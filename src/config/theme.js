eab0d3813c7ab150d1639a115c0d3e7e70485ebd
于连林520wcf <yll@520wcf.com>
2018-05-31 20:56:56 +0800

    适配 iPhoneX 机型

--- src/config/theme.js
+++ src/config/theme.js
@@ -9,10 +9,22 @@ export default module = {
         height: 44,//Platform.OS === 'android' ? 56 : 44 //根据不通平台高度不一致
         backgroundColor: '#fff'
     },
-    barContentPad: (Platform.OS === 'android' ? 0 : 20), //根据不同平台顶部 padding不一致
+    barContentPad: (Platform.OS === 'android' ? 0 : (isIphoneX() ? 42 : 20)),
+    bottomPadding: isIphoneX() ? 18 : 0,
     // 常用颜色
     primaryColor: '#EE0000',
     lightGray: '#f5f5f5',
     darkGray: '#e5e5e5',
     lightBlack: '#333333'
 };
+
+
+export function isIphoneX() {
+    let dimension = Dimensions.get('window');
+    return (
+        Platform.OS === 'ios' &&
+        !Platform.isPad &&
+        !Platform.isTVOS &&
+        (dimension.height === 812 || dimension.width === 812)
+    );
+}
